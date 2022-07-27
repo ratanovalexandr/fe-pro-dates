9d962530bdbcd2f1300aadf95b437687c7630e08
ratanovalexandr <ratanovalexandr@gmail.com>
2022-07-27 16:44:52 +0300

    add index.js

--- js/dates.js
+++ js/dates.js
@@ -23,23 +23,19 @@ function getDay(date, lang) {
       6: 'Суббота',
     },
   };
-  const date1 = new Date(date);
-  const date2 = date1.getDay();
-
-  if (lang == 'en') {
-    return dayNames.en[date2];
-  }
-  if (lang == 'ru') {
-    return dayNames.ru[date2];
-  }
+  return dayNames[lang][date.getDay()];
 }
 
 // Принимает объект даты, и должно вернуть компоненты даты в виде строки.
 // Вид должен быть такой 12:02(часы и минуты), то есть если у вас одно число на одном из
 // компонентов, то добавляем 0 перед ним
 function formatTime(date) {
-  return date.toLocaleTimeString('en-GB').slice(0, -3);
-  // return '${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}';
+  // return date.toLocaleTimeString('en-GB').slice(0, -3);
+
+  return ` ${date.getHours().toString().padStart(2, '0')}:${date
+    .getMinutes()
+    .toString()
+    .padStart(2, '0')}`;
 }
 
 /*
@@ -52,9 +48,7 @@ month – месяц от 0 до 11.
 К примеру, getLastDayOfMonth(2012, 1) = 29 (високосный год, февраль).
 */
 function getLastDayOfMonth(year, month) {
-  let date1 = new Date(year, month);
-  let date2 = new Date(year, month + 1);
-  return (date2 - date1) / 1000 / 3600 / 24;
+  return new Date(year, month + 1, 0).getDate();
 }
 
 module.exports = {
